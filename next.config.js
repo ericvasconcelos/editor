f9be51210b64bbf29580ec94cc02affa30af595a
Eric Oliveira <eric.oliveira@cashme.com.br>
2022-08-20 11:01:16 -0300

    Update next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  productionBrowserSourceMaps: true,
+  compiler: {
+    styledComponents: true,
+    reactRemoveProperties: true
+  }
 }
 
 module.exports = nextConfig
